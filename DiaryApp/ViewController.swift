7014b7672e2c7400ad494a27db2e1c88ff48a713
Yun, Seongeun <0420yun@gamil.com>
2023-09-23 16:58:20 +0900

    Initial Commit

---
+++ DiaryApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DiaryApp
+//
+//  Created by 윤성은 on 2023/09/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
